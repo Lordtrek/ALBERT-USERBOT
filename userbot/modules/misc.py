ca361bfb11d19fc35bc648a85720a09335a1b6f0
Bagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2022-01-15 23:02:56 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -63,7 +63,7 @@ async def sleepybot(time):
 @register(outgoing=True, pattern="^.shutdown$")
 async def killdabot(event):
     """For .shutdown command, shut the bot down."""
-    await event.edit("**Mematikan Skyzu-Userbot....**")
+    await event.edit("**Mematikan ALBERT-USERBOT....**")
     await asyncio.sleep(7)
     await event.delete()
     if BOTLOG:
@@ -75,7 +75,7 @@ async def killdabot(event):
 
 @register(outgoing=True, pattern="^.restart$")
 async def killdabot(event):
-    await event.edit("**Restarting Skyzu-Userbot...**")
+    await event.edit("**Restarting ALBERT-USERBOT...**")
     await asyncio.sleep(10)
     await event.delete()
     if BOTLOG:
@@ -93,7 +93,7 @@ async def killdabot(event):
 async def reedme(e):
     await e.edit(
         "Here's Something for You to Read :\n"
-        "\n[⚡ Skyzu-UserBot Repo](https://github.com/Askarbot/Skyzuu-Userbot/blob/skyzu-userBot/README.md)"
+        "\n[🎭𝐀𝐋𝐁𝐄𝐑𝐓-𝐔𝐒𝐄𝐑𝐁𝐎𝐓🎭](https://github.com/Lordtrek/ALBERT-USERBOT/blob/ALBERT-USERBOT/README.md)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
         "\n[Special - Note](https://telegra.ph/Special-Note-11-02)"
     )
@@ -119,9 +119,9 @@ async def repo_is_here(wannasee):
     await wannasee.edit(
         "**Usᴇʀʙᴏᴛ Tᴇʟᴇɢʀᴀᴍ**\n"
         "𝗥𝗲𝗽𝗼 🇮🇩\n"
-        "╰⎆ [𝐒𝐊𝐘𝐙𝐔-𝐔𝐒𝐄𝐑𝐁𝐎𝐓​](https://github.com/Skyzu/skyzu-userbot)\n"
-        "❏ **Oᴡɴᴇʀ​** ⎆ [Skyzu](t.me/skyzu)\n"
-        "❏ **Sᴜᴘᴘᴏʀᴛ**​ ⎆ [groups](t.me/skyzusupport)\n"
+        "╰⎆ [🎭𝐀𝐋𝐁𝐄𝐑𝐓-𝐔𝐒𝐄𝐑𝐁𝐎𝐓🎭​](https://github.com/Lordtrek/ALBERT-USERBOT)\n"
+        "❏ **Oᴡɴᴇʀ​** ⎆ [ᴀʟʙᴇʀᴛ](t.me/AlbertEinsteinih)\n"
+        "❏ **Sᴜᴘᴘᴏʀᴛ**​ ⎆ [sᴜᴘᴘᴏʀᴛ](t.me/suportalbert)\n"
     )
 
 
