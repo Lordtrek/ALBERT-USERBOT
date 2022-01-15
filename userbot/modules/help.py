dc962dac932befe78d3c839baff7c9381fe144cf
Bagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2022-01-15 22:41:56 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -35,7 +35,7 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t ❉  "
         await event.edit(
-            "**⚡ ALBERT-USERBOT⚡**\n\n"
+            "**🎭𝐀𝐋𝐁𝐄𝐑𝐓-𝐔𝐒𝐄𝐑𝐁𝐎𝐓🎭**\n\n"
             f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
             "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
             f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n✐  support : @suportalbert"
