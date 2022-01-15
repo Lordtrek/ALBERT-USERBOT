51c07e572e4880d89c85e0d4e2ef3a5cde749cfe
Bagaskara12 <93908715+ybgskr12@users.noreply.github.com>
2022-01-15 23:08:57 +0700

    Update skyhelper.py

--- userbot/modules/skyhelper.py
+++ userbot/modules/skyhelper.py
@@ -11,23 +11,23 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/skyzuuuu)"
-        "\n[Repo](https://github.com/Skyzu/skyzu-userbot)"
-    )
+        "\n[Telegram](t.me/AlbertEinsteinih)"
+        "\n[Repo](https://github.com/Lordtrek/ALBERT-USERBOT)"
+   
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Skyzu/skyzu-userbot/skyzu-userbot/varshelper.txt)"
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Lordtrek/ALBERT-USERBOT/ALBERT-USERBOT/varshelper.txt)"
     )
 
 
 CMD_HELP.update(
     {
         "helper": "`.lhelp`\
-\nUsage: Bantuan Untuk Skyzuu-Userbot.\
+\nUsage: Bantuan Untuk ALBERT-USERBOT.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
     }
